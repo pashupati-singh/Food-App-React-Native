716993a0a76d4304f1e527d489c2c09b12539e14
pashupati-singh <119448686+pashupati-singh@users.noreply.github.com>
2024-06-15 15:06:49 +0530

    last

---
+++ app/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Navbar } from '../Components/Navbar'
+
+export const HomePage = () => {
+  return (
+    <View>
+        <Navbar />
+    </View>
+  )
+}
